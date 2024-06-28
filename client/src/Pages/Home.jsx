e746b0e10a7722448ce0b1b8bd86c547fef7498d
Ritwik <ritwikbhowmik659@gmail.com>
2024-06-28 21:34:23 +0530

    Add Image card Component

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "../Components/HeroSection"
+import PhotoGallary from "../Components/PhotoGallary"
 
 
 const Home = () => {
   return (
     <div className="mt-36">
               <HeroSection/>
+              <PhotoGallary/>
     </div>
   
   )
